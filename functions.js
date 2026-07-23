3e26b7e817c8106e17e2669a0c20038821c9df62
THE-DON007 <russelldon614@gmail.com>
2026-07-23 11:22:00 +0100

    Add reusable functionsto find the largest of three numbers

--- functions.js
+++ functions.js
@@ -2,3 +2,7 @@
 function isEven(number) {
     return number % 2 === 0;
 }
+// 2. Finds the largest value among three numbers
+function findLargest(firstNumber, secondNumber, thirdNumber) {
+    return Math.max(firstNumber, secondNumber, thirdNumber);
+}
